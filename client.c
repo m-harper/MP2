b5308a429d956793e9861fec44f8b59149ddd449
m-harper <matthew.harper@tamu.edu>
2013-02-16 09:57:13 -0600

    Local run finished

--- client.c
+++ client.c
@@ -1,4 +1,6 @@
 #include "reqchannel.h"
+#include "sstream"
+#include <cstdlib>
 
 /*
 The client program (to be implemented as program client.C is to fork off a process, which is
@@ -18,7 +20,38 @@ compared to handling the request in a local function.
 
 using namespace std;
 
+// Returns if the program should fork or run locally
+bool handle_args(int, char**);
+
+// Function for running locally
+string process_request(string);
+
+string int2string(int);
+
 int main(int argcs, char** argv) {
+	bool run_mode = handle_args(argcs, argv);
+	
+	if (run_mode) {
+		// Run forked
+	}
+	else {
+		// Run locally
+		string rep1, rep2, rep3;
+		
+		// Send a number of requests
+		rep1 = process_request("hello");
+		cout << "Reply to request 'hello' is " << rep1 << endl;
+		
+		rep2 = process_request("data Joe Smith");
+		cout << "Reply to request 'data Joe Smith' is " << rep2 << endl;
+		
+		rep3 = process_request("quit");
+		cout << "Reply to request 'quit' is " << rep3 << endl;
+				
+		if (rep3 == "bye") {
+			exit(0);
+		}
+	}
 	// First fork
 	
 	// Exec dataserver
@@ -28,4 +61,52 @@ int main(int argcs, char** argv) {
 	// Before terminating, send quit request and wait for bye
 	
 	return 0;
+}
+
+/*
+ *  Returns if the program should fork or run locally
+ *  True means fork, false, run locally
+ *  If no arguments given (or incorrect arguements), return true to fork
+ */
+bool handle_args(int argc, char** argv) {
+	// No arguement given, return true
+	if (argc == 1) {
+		return true;
+	}	
+	else if (argc == 2) {
+		// Correct number of arguements given
+		char arg = *(argv[1]);
+		if (arg == '1')
+			return false;
+		if (arg == '2')
+			return true;
+		// Arguement was incorrect, return true by default
+		cerr << "Unknown arguement " << arg << ", defaulting to fork" << endl;
+		return true;
+	}
+	else {
+		cerr << "Incorrect number of arguements, defaulting to fork" << endl;
+		return true;
+	}
+}
+
+string process_request(string _request) {
+	if (_request == "hello") {
+		return "hello to you too";
+	}
+	else if (_request.substr(0,4) == "data") {
+		return int2string(rand() % 100);
+	}
+	else if (_request == "quit") {
+		return "bye";
+	}
+	else {
+		return "unknown request";
+	}
+}
+
+string int2string(int number) {
+   stringstream ss;
+   ss << number;
+   return ss.str();
 }
